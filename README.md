96e0fa80f408c4ee28237bd27af4fce054dac4b7
Owen_study <owen_study@126.com>
2017-06-13 19:46:29 -0700

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # awesome-web-app
+--2017.6.14 finish the task day1, day2
--- www/app.py
+++ www/app.py
@@ -10,12 +10,20 @@ def index(request):
 
 @asyncio.coroutine
 def init(loop):
+    #app 应用初始化
     app=web.Application(loop=loop)
+    #路由解析
     app.router.add_route('GET','/',index)
+    #设置服务器端口
     srv=yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    #LOG信息
     logging.info('server started at http://127.0.0.1:9000...')
+
     return srv
 
+#得到循环事件
 loop=asyncio.get_event_loop()
+#初始化循环事件
 loop.run_until_complete(init(loop))
+#循环启动，开始接收客户端响应
 loop.run_forever()
